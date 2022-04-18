cd1ef0b5b9badfafde1927f3ee8f7dab2a5d2e20
Oleg Omelchenko <olegoriginal@yandex.ru>
2022-04-18 15:00:29 +0300

    navbar fix

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -362,6 +362,15 @@ const Navbar = () => {
           ) : null}
           {auth.roles.includes('bookkeeper') ? (
             <NavLink
+              to="/stoprice/list/rus"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-blue-700 mr-4"
+              activeClassName="text-blue-600 underline font-semibold"
+            >
+              Цены (СТО)
+            </NavLink>
+          ) : null}
+          {auth.roles.includes('bookkeeper') ? (
+            <NavLink
               to="/material/list"
               className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-blue-700 mr-4"
               activeClassName="text-blue-600 underline font-semibold"
