8bc14a867faf80ab44244640539aa1e4469bc37e
Oleg Omelchenko <oleg.omelc93@gmail.com>
2023-09-10 20:53:46 +0300

    [security] - report password

---
+++ client/scenes/Report/PasswordChecker.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+
+const PasswordChecker = ({ pass, setPasswordIsCorrect }) => {
+  toast.configure()
+  const notify = (arg) => {
+    toast.info(arg, { position: toast.POSITION.BOTTOM_RIGHT })
+  }
+
+  const [password, setPassword] = useState('')
+
+  const onChangePassowrd = (e) => {
+    const { value } = e.target
+    setPassword(value)
+  }
+
+  const submitPassword = () => {
+    if (password === pass) {
+      setPasswordIsCorrect(true)
+      notify('Доступ разблокирован')
+    } else {
+      setPasswordIsCorrect(false)
+      notify('Пароль неверный')
+    }
+  }
+
+  return (
+    <div className="w-screen mt-5 pt-5 bg-gray-200 flex justify-center items-center">
+      <div className=" max-w-xs ">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Пароль
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mask-password"
+              type="text"
+              name="password"
+              value={password}
+              onChange={onChangePassowrd}
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={submitPassword}
+            >
+              Перейти к отчетам
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PasswordChecker
--- client/scenes/Report/Report.js
+++ client/scenes/Report/Report.js
@@ -4,6 +4,7 @@ import cx from 'classnames'
 import Navbar from '../../components/Navbar'
 import ReportSidebar from './Report.sidebar'
 import Shinomontazh from './shinomontazh/wrapper'
+import PasswordChecker from './PasswordChecker'
 
 const Autoparts = React.lazy(() => import('./Autoparts/wrapper'))
 
@@ -11,6 +12,9 @@ const Report = () => {
   const placeList = useSelector((s) => s.places.list)
   const employeeList = useSelector((s) => s.employees.list)
   const auth = useSelector((s) => s.auth)
+
+  const mockPassword = '320208'
+  const [passwordIsCorrect, setPasswordIsCorrect] = useState(false)
 
   const [checkIsAdmin, setCheckIsAdmin] = useState(true)
 
@@ -48,184 +52,185 @@ const Report = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-row">
-        <ReportSidebar
-          setActiveMonth={setActiveMonth}
-          setActiveDay={setActiveDay}
-          activeDay={activeDay}
-          activeMonth={activeMonth}
-          placeList={placeList}
-          place={place}
-          onChangePlace={onChangePlace}
-          calendarType={calendarType}
-          setCalendarType={setCalendarType}
-          checkIsAdmin={checkIsAdmin}
-          timeStart={timeStart}
-          onChangeTimeStart={onChangeTimeStart}
-          timeFinish={timeFinish}
-          onChangeTimeFinish={onChangeTimeFinish}
-          range={range}
-          setRange={setRange}
-          active={active}
-          auth={auth}
-        />
-        <div className="w-full mx-auto px-4">
-          <h1 className="text-3xl py-4 border-b mb-6">Статистика</h1>
-          <div className="py-3 px-4 my-3 rounded-lg shadow bg-white">
-            <div className="flex flex-wrap">
-              {auth.roles.includes('kassa') || auth.roles.includes('boss') ? (
-                <>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'sh-kassa',
-                        'border-b-8 border-blue-400': active === 'sh-kassa'
-                      })}
-                      onClick={() => setActive('sh-kassa')}
-                    >
-                      Шиномонтаж (касса)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'sto-kassa',
-                        'border-b-8 border-blue-400': active === 'sto-kassa'
-                      })}
-                      onClick={() => setActive('sto-kassa')}
-                    >
-                      СТО (касса)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'wash-kassa',
-                        'border-b-8 border-blue-400': active === 'wash-kassa'
-                      })}
-                      onClick={() => setActive('wash-kassa')}
-                    >
-                      Мойка (касса)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'window-kassa',
-                        'border-b-8 border-blue-400': active === 'window-kassa'
-                      })}
-                      onClick={() => setActive('window-kassa')}
-                    >
-                      Стекла (касса)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'cond-kassa',
-                        'border-b-8 border-blue-400': active === 'cond-kassa'
-                      })}
-                      onClick={() => setActive('cond-kassa')}
-                    >
-                      Кондиционеры (касса)
-                    </button>
-                  </div>
-                </>
-              ) : null}
-              {auth.roles.includes('bookkeeper') || auth.roles.includes('boss') ? (
-                <>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'sh-buh',
-                        'border-b-8 border-blue-400': active === 'sh-buh'
-                      })}
-                      onClick={() => setActive('sh-buh')}
-                    >
-                      Шиномонтаж (бухгалтерия)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'sto-buh',
-                        'border-b-8 border-blue-400': active === 'sto-buh'
-                      })}
-                      onClick={() => setActive('sto-buh')}
-                    >
-                      СТО (бухгалтерия)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'wash-buh',
-                        'border-b-8 border-blue-400': active === 'wash-buh'
-                      })}
-                      onClick={() => setActive('wash-buh')}
-                    >
-                      Мойка (бухгалтерия)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'window-buh',
-                        'border-b-8 border-blue-400': active === 'window-buh'
-                      })}
-                      onClick={() => setActive('window-buh')}
-                    >
-                      Стекла (бухгалтерия)
-                    </button>
-                  </div>
-                  <div className="w-1/5 p-2">
-                    <button
-                      type="button"
-                      className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                        block: active !== 'cond-buh',
-                        'border-b-8 border-blue-400': active === 'cond-buh'
-                      })}
-                      onClick={() => setActive('cond-buh')}
-                    >
-                      Кондиционеры (бухгалтерия)
-                    </button>
-                  </div>
-                </>
-              ) : null}
-              <div className="w-1/5 p-2">
-                <button
-                  type="button"
-                  className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                    block: active !== 'material',
-                    'border-b-8 border-blue-400': active === 'material'
-                  })}
-                  onClick={() => setActive('material')}
-                >
-                  Материалы (шиномонтаж)
-                </button>
-              </div>
-              <div className="w-1/5 p-2">
-                <button
-                  type="button"
-                  className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
-                    block: active !== 'autopart',
-                    'border-b-8 border-blue-400': active === 'autopart'
-                  })}
-                  onClick={() => setActive('autopart')}
-                >
-                  Автозапчасти
-                </button>
-              </div>
-              {/* {checkIsAdmin ? (
+      {passwordIsCorrect ? (
+        <div className="flex flex-row">
+          <ReportSidebar
+            setActiveMonth={setActiveMonth}
+            setActiveDay={setActiveDay}
+            activeDay={activeDay}
+            activeMonth={activeMonth}
+            placeList={placeList}
+            place={place}
+            onChangePlace={onChangePlace}
+            calendarType={calendarType}
+            setCalendarType={setCalendarType}
+            checkIsAdmin={checkIsAdmin}
+            timeStart={timeStart}
+            onChangeTimeStart={onChangeTimeStart}
+            timeFinish={timeFinish}
+            onChangeTimeFinish={onChangeTimeFinish}
+            range={range}
+            setRange={setRange}
+            active={active}
+            auth={auth}
+          />
+          <div className="w-full mx-auto px-4">
+            <h1 className="text-3xl py-4 border-b mb-6">Статистика</h1>
+            <div className="py-3 px-4 my-3 rounded-lg shadow bg-white">
+              <div className="flex flex-wrap">
+                {auth.roles.includes('kassa') || auth.roles.includes('boss') ? (
+                  <>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'sh-kassa',
+                          'border-b-8 border-blue-400': active === 'sh-kassa'
+                        })}
+                        onClick={() => setActive('sh-kassa')}
+                      >
+                        Шиномонтаж (касса)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'sto-kassa',
+                          'border-b-8 border-blue-400': active === 'sto-kassa'
+                        })}
+                        onClick={() => setActive('sto-kassa')}
+                      >
+                        СТО (касса)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'wash-kassa',
+                          'border-b-8 border-blue-400': active === 'wash-kassa'
+                        })}
+                        onClick={() => setActive('wash-kassa')}
+                      >
+                        Мойка (касса)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'window-kassa',
+                          'border-b-8 border-blue-400': active === 'window-kassa'
+                        })}
+                        onClick={() => setActive('window-kassa')}
+                      >
+                        Стекла (касса)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'cond-kassa',
+                          'border-b-8 border-blue-400': active === 'cond-kassa'
+                        })}
+                        onClick={() => setActive('cond-kassa')}
+                      >
+                        Кондиционеры (касса)
+                      </button>
+                    </div>
+                  </>
+                ) : null}
+                {auth.roles.includes('bookkeeper') || auth.roles.includes('boss') ? (
+                  <>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'sh-buh',
+                          'border-b-8 border-blue-400': active === 'sh-buh'
+                        })}
+                        onClick={() => setActive('sh-buh')}
+                      >
+                        Шиномонтаж (бухгалтерия)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'sto-buh',
+                          'border-b-8 border-blue-400': active === 'sto-buh'
+                        })}
+                        onClick={() => setActive('sto-buh')}
+                      >
+                        СТО (бухгалтерия)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'wash-buh',
+                          'border-b-8 border-blue-400': active === 'wash-buh'
+                        })}
+                        onClick={() => setActive('wash-buh')}
+                      >
+                        Мойка (бухгалтерия)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'window-buh',
+                          'border-b-8 border-blue-400': active === 'window-buh'
+                        })}
+                        onClick={() => setActive('window-buh')}
+                      >
+                        Стекла (бухгалтерия)
+                      </button>
+                    </div>
+                    <div className="w-1/5 p-2">
+                      <button
+                        type="button"
+                        className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                          block: active !== 'cond-buh',
+                          'border-b-8 border-blue-400': active === 'cond-buh'
+                        })}
+                        onClick={() => setActive('cond-buh')}
+                      >
+                        Кондиционеры (бухгалтерия)
+                      </button>
+                    </div>
+                  </>
+                ) : null}
+                <div className="w-1/5 p-2">
+                  <button
+                    type="button"
+                    className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                      block: active !== 'material',
+                      'border-b-8 border-blue-400': active === 'material'
+                    })}
+                    onClick={() => setActive('material')}
+                  >
+                    Материалы (шиномонтаж)
+                  </button>
+                </div>
+                <div className="w-1/5 p-2">
+                  <button
+                    type="button"
+                    className={cx('p-4 bg-gray-200 rounded w-full h-full overflow-hidden', {
+                      block: active !== 'autopart',
+                      'border-b-8 border-blue-400': active === 'autopart'
+                    })}
+                    onClick={() => setActive('autopart')}
+                  >
+                    Автозапчасти
+                  </button>
+                </div>
+                {/* {checkIsAdmin ? (
                 <div className="w-1/5 p-2">
                   <a
                     href="https://docs.google.com/spreadsheets/d/1L6mR3e0bmu13y7OqIlMQbziWb4a7Ev0k9X_zu-kEBTg/edit?usp=sharing"
@@ -237,44 +242,47 @@ const Report = () => {
                   </a>
                 </div>
               ) : null} */}
-            </div>
-            <Shinomontazh
-              calendarType={calendarType}
-              place={place}
-              activeMonth={activeMonth}
-              activeDay={activeDay}
-              timeFinish={timeFinish}
-              employeeList={employeeList}
-              timeStart={timeStart}
-              active={active}
-              range={range}
-              // auth={auth}
-            />
-            {active === 'autopart' ? (
-              <div
-                className={cx('', {
-                  block: active === 'autopart',
-                  hidden: active !== 'autopart'
-                })}
-              >
-                {' '}
-                <Suspense fallback={<div>Загрузка...</div>}>
-                  <Autoparts
-                    calendarType={calendarType}
-                    place={place}
-                    activeMonth={activeMonth}
-                    activeDay={activeDay}
-                    timeFinish={timeFinish}
-                    employeeList={employeeList}
-                    timeStart={timeStart}
-                    active={active}
-                  />
-                </Suspense>
               </div>
-            ) : null}
+              <Shinomontazh
+                calendarType={calendarType}
+                place={place}
+                activeMonth={activeMonth}
+                activeDay={activeDay}
+                timeFinish={timeFinish}
+                employeeList={employeeList}
+                timeStart={timeStart}
+                active={active}
+                range={range}
+                // auth={auth}
+              />
+              {active === 'autopart' ? (
+                <div
+                  className={cx('', {
+                    block: active === 'autopart',
+                    hidden: active !== 'autopart'
+                  })}
+                >
+                  {' '}
+                  <Suspense fallback={<div>Загрузка...</div>}>
+                    <Autoparts
+                      calendarType={calendarType}
+                      place={place}
+                      activeMonth={activeMonth}
+                      activeDay={activeDay}
+                      timeFinish={timeFinish}
+                      employeeList={employeeList}
+                      timeStart={timeStart}
+                      active={active}
+                    />
+                  </Suspense>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <PasswordChecker setPasswordIsCorrect={setPasswordIsCorrect} pass={mockPassword} />
+      )}
     </div>
   )
 }
