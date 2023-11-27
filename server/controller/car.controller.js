087fa1ab3e73f6a193e802f5db5f99c738f0c349
Oleg Omelchenko <oleg.omelc93@gmail.com>
2023-11-27 21:59:58 +0300

    [car models] - new routes

--- server/controller/car.controller.js
+++ server/controller/car.controller.js
@@ -4,19 +4,34 @@ const CarGeneration = require('../model/car.generation')
 const CarModification = require('../model/car.modification')
 const CarSerie = require('../model/car.serie')
 
+const ACCESS_DEMO_KEY = 'dljdfIHCCU(*HY&!'
+
 exports.getAllMarks = async (req, res) => {
   const list = await CarMark.find({})
   return res.json({ status: 'ok', data: list })
 }
-
 exports.createMark = async (req, res) => {
   const car = new CarMark(req.body)
   await car.save()
   return res.json({ status: 'ok', data: car })
 }
+exports.createManyMarks = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    const car = await CarMark.insertMany(req.body)
+    return res.json({ status: 'ok', data: car })
+  }
+  return res.json({ status: 'error' })
+}
 exports.getOneMark = async (req, res) => {
   const car = await CarMark.findOne({ id_car_mark: req.params.id })
   return res.json({ status: 'ok', data: car })
+}
+exports.deleteAllMarks = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    await CarMark.deleteMany({})
+    return res.json({ status: 'ok' })
+  }
+  return res.json({ status: 'error' })
 }
 
 exports.getAllModels = async (req, res) => {
@@ -28,9 +43,23 @@ exports.createModel = async (req, res) => {
   await car.save()
   return res.json({ status: 'ok', data: car })
 }
+exports.createManyModels = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    const car = await CarModel.insertMany(req.body)
+    return res.json({ status: 'ok', data: car })
+  }
+  return res.json({ status: 'error' })
+}
 exports.getModelbyMark = async (req, res) => {
   const car = await CarModel.find({ id_car_mark: req.params.id })
   return res.json({ status: 'ok', data: car })
+}
+exports.deleteAllModels = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    await CarModel.deleteMany({})
+    return res.json({ status: 'ok' })
+  }
+  return res.json({ status: 'error' })
 }
 
 exports.getAllGens = async (req, res) => {
@@ -42,9 +71,23 @@ exports.createGen = async (req, res) => {
   await car.save()
   return res.json({ status: 'ok', data: car })
 }
+exports.createManyGens = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    const car = await CarGeneration.insertMany(req.body)
+    return res.json({ status: 'ok', data: car })
+  }
+  return res.json({ status: 'error' })
+}
 exports.getGenbyModel = async (req, res) => {
   const car = await CarGeneration.find({ id_car_model: req.params.id })
   return res.json({ status: 'ok', data: car })
+}
+exports.deleteAllGens = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    await CarGeneration.deleteMany({})
+    return res.json({ status: 'ok' })
+  }
+  return res.json({ status: 'error' })
 }
 
 exports.getAllMods = async (req, res) => {
@@ -55,6 +98,21 @@ exports.getModbyGen = async (req, res) => {
   const car = await CarModification.find({ id_car_model: req.params.id })
   return res.json({ status: 'ok', data: car })
 }
+exports.createManyMods = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    const car = await CarModification.insertMany(req.body)
+    return res.json({ status: 'ok', data: car })
+  }
+  return res.json({ status: 'error' })
+}
+exports.deleteAllMods = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    await CarModification.deleteMany({})
+    return res.json({ status: 'ok' })
+  }
+  return res.json({ status: 'error' })
+}
+
 exports.getSerbyGen = async (req, res) => {
   const car = await CarSerie.find({ id_car_generation: req.params.id })
   return res.json({ status: 'ok', data: car })
@@ -63,4 +121,18 @@ exports.createSer = async (req, res) => {
   const car = new CarSerie(req.body)
   await car.save()
   return res.json({ status: 'ok', data: car })
+}
+exports.createManySeries = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    const car = await CarSerie.insertMany(req.body)
+    return res.json({ status: 'ok', data: car })
+  }
+  return res.json({ status: 'error' })
+}
+exports.deleteAllSeries = async (req, res) => {
+  if (req.params.key === ACCESS_DEMO_KEY) {
+    await CarSerie.deleteMany({})
+    return res.json({ status: 'ok' })
+  }
+  return res.json({ status: 'error' })
 }
--- server/routes/api/car.routes.js
+++ server/routes/api/car.routes.js
@@ -3,18 +3,37 @@ const carController = require('../../controller/car.controller')
 
 const router = express.Router()
 
+// car_mark.csv
 router.get('/carmark/', carController.getAllMarks)
 router.get('/carmark/:id', carController.getOneMark)
 router.post('/carmark/', carController.createMark)
+router.post('/carmark/:key', carController.createManyMarks)
+router.delete('/carmark/:key', carController.deleteAllMarks)
+
+// car_model.csv
 router.get('/carmodels/', carController.getAllModels)
 router.post('/carmodels/:id', carController.createModel)
 router.get('/carmodel/:id', carController.getModelbyMark)
+router.post('/carmodel/:key', carController.createManyModels)
+router.delete('/carmodel/:key', carController.deleteAllModels)
+
+// car_generation.csv
 router.get('/cargens', carController.getAllGens)
 router.post('/cargen/', carController.createGen)
 router.get('/cargen/:id', carController.getGenbyModel)
+router.post('/cargen/:key', carController.createManyGens)
+router.delete('/cargen/:key', carController.deleteAllGens)
+
+// car_modification.csv
 router.get('/carmods', carController.getAllMods)
 router.get('/carmod/:id', carController.getModbyGen)
+router.post('/carmod/:key', carController.createManyMods)
+router.delete('/carmod/:key', carController.deleteAllMods)
+
+// car_serie.csv
 router.get('/carserie/:id', carController.getSerbyGen)
-router.post('/carserie/:id', carController.createSer)
+router.post('/carserie/', carController.createSer)
+router.post('/carserie/:key', carController.createManySeries)
+router.delete('/carserie/:key', carController.deleteAllSeries)
 
 module.exports = router
