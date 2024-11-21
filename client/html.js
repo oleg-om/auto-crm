2e77247a9d6f3983be08ae107cf43d19982fcf05
Oleg Omelchenko <oleg.omelc93@gmail.com>
2024-11-21 17:12:40 +0300

    study version

--- client/html.js
+++ client/html.js
@@ -6,8 +6,10 @@ const Html = ({ body }) => {
       <meta charset="UTF-8">
       <link rel="stylesheet" type="text/css" href="/css/main.css" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Autodom CRM</title>
-      <link rel="icon" href="/images/icon.png" />
+      <title>Autodom ${process.env?.MODE === 'study' ? 'Обучение' : 'CRM'}</title>
+      <link rel="icon" href=${
+        process.env?.MODE === 'study' ? '/images/icon-2.png' : '/images/icon.png'
+      } />
       <meta name="robots" content="noindex" />
     </head>
     <body>
