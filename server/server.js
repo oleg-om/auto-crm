bf278d38afdae3a715b40b62a5c8a061ec1ff426
Oleg Omelchenko <oleg.omelc93@gmail.com>
2025-03-17 11:11:13 +0300

    parse tyres fix

--- server/server.js
+++ server/server.js
@@ -457,5 +457,5 @@ io.on('connection', (socket) => {
 console.log(`Serving at http://localhost:${port}`)
 
 if (process.env.MODE !== 'study') {
-  setInterval(() => kerchshinaCheck(io), 10000)
+  setInterval(() => kerchshinaCheck(io), 40000)
 }
