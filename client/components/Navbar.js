25c1398b618a84f9fe52ce51fc3ca29ddd5c104b
oleg-om <olegoriginal@yandex.ru>
2026-09-11 16:19:30 +0300

    redesign

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,8 +2,16 @@ import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import cx from 'classnames'
 import { useSelector, useDispatch } from 'react-redux'
+import { LogOut, User } from 'lucide-react'
 // import dotenv from 'dotenv'
 import { signOut } from '../redux/reducers/auth'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from './ui/dropdown-menu'
+import { Button } from './ui/button'
 
 import { socket } from '../redux/sockets/socketReceivers'
 
@@ -19,13 +27,6 @@ const Navbar = () => {
       return setToggle(true)
     }
     return setToggle(false)
-  }
-  const [dropdown, setDropdown] = useState(false)
-  const DropdownOpen = () => {
-    if (dropdown === false) {
-      return setDropdown(true)
-    }
-    return setDropdown(false)
   }
 
   socket.connect()
@@ -500,77 +501,20 @@ const Navbar = () => {
             ) : null}
           </div>
         )}
-        <div className="relative inline-block text-left">
-          <div>
-            <button
-              type="button"
-              className="flex bg-gray-100 font-normal hover:bg-gray-300 text-gray-700 py-2 px-4 rounded items-center md:mt-0 mt-4"
-              id="options-menu"
-              aria-haspopup="true"
-              aria-expanded="true"
-              onClick={DropdownOpen}
-            >
-              <svg
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                className="mb-auto mt-auto mr-1"
-                viewBox="0 0 28 28"
-              >
-                <g>
-                  <path
-                    d="M18.486,18.104c1.034-1.013,1.775-2.531,2.115-4.648c0.455,0.083,1.053-0.387,1.261-1.179
-		c0.214-0.813,0.462-2.178-0.003-2.302c-0.137-0.037-0.279-0.009-0.422,0.063v-2.26c0.076-2.457-0.26-4.829-2.939-5.856
-		C18.009,0.915,17.977,0,17.977,0c-1.206,1.086-5.785,1.526-5.785,1.526l0.052,0.014C6.438,2.137,7.276,4.224,7.276,7.777v2.26
-		C7.134,9.965,6.992,9.936,6.854,9.974c-0.463,0.124-0.323,1.437-0.109,2.251c0.209,0.798,0.921,1.321,1.377,1.229
-		c0.347,2.223,1.041,3.703,1.995,4.67c-4.443,1.625-7.604,5.728-7.604,10.543h23.641C26.152,23.827,22.962,19.71,18.486,18.104z"
-                  />
-                </g>
-              </svg>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" className="mt-4 gap-2 font-normal md:mt-0">
+              <User className="h-4 w-4" />
               {auth.user.login}
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-          <div
-            className={cx('absolute right-0 mt-2 w-32 rounded-md shadow-lg bg-white z-50', {
-              hidden: dropdown === false,
-              block: dropdown === true
-            })}
-          >
-            <div
-              className="py-1"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="options-menu"
-            >
-              <button
-                className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                type="button"
-                role="menuitem"
-                onClick={() => {
-                  dispatch(signOut())
-                }}
-              >
-                Выйти
-              </button>
-            </div>
-          </div>
-        </div>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => dispatch(signOut())}>
+              <LogOut className="h-4 w-4" />
+              Выйти
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   )
